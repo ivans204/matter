75449b0a6716011e65b5ec7fc394ce8c78576709
ivans204 <ivansvrga204@gmail.com>
2019-03-12 15:27:28 +0100

    horizontal and verical

--- JS/main.js
+++ JS/main.js
@@ -11,10 +11,12 @@ const Constraint = Matter.Constraint;
 const MouseConstraint = Matter.MouseConstraint;
 const Mouse = Matter.Mouse;
 const World = Matter.World;
+const Vertices = Matter.Vertices;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const SAT = Matter.SAT;
 const Bounds = Matter.Bounds;
+const Svg = Matter.Svg;
 
 //== Engine creation
 let engine = Engine.create();
@@ -31,26 +33,35 @@ function createRect(middleX, middleY, width, height, options = 0) {
     return Bodies.rectangle(middleX, middleY, width, height, options)
 }
 
+function createPolygon(x, y, sides, radius, options) {
+    return Bodies.polygon(x, y, sides, radius, options)
+}
+
 function draw(element) {
     World.add(world, element)
 }
 
 function wallTop(width, height = 0) {
-    return createRect(width / 2, -20, width, 50, {isStatic: true, restitution: 0, render: {fillStyle: '#333'}});
+    return createRect(width / 2, -20, width, 50, {
+        isStatic: true, restitution: 0, friction: 0, frictionAir: 0, render: {fillStyle: '#333'}
+    });
 }
 
 function wallBot(width, height) {
-    return createRect(width / 2, height + 20, width, 50, {isStatic: true, restitution: 0, render: {fillStyle: '#333'}});
+    return createRect(width / 2, height + 20, width, 50, {
+        isStatic: true, restitution: 0, friction: 0, frictionAir: 0, render: {fillStyle: '#333'}
+    });
 }
 
 function wallLeft(width, height) {
-    return createRect(-20, height / 2, 50, height, {isStatic: true, restitution: 0, render: {fillStyle: '#333'}});
+    return createRect(-20, height / 2, 50, height, {
+        isStatic: true, restitution: 0, friction: 0, frictionAir: 0, render: {fillStyle: '#333'}
+    });
 }
 
 function wallRight(width, height) {
     return createRect(width + 20, height / 2, 50, height, {
-        isStatic: true,
-        render: {fillStyle: '#333', restitution: 0}
+        isStatic: true, restitution: 0, friction: 0, frictionAir: 0, render: {fillStyle: '#333'}
     });
 }
 
@@ -95,19 +106,25 @@ project.restitution = function () {
     draw([topWall,]);
 
     col[0].addEventListener('click', () => {
-        let ele = createCircle(400, 250, 25, {restitution: 0.9, render: {fillStyle: '#b6f27d'}});
+        let ele = createCircle(400, 250, 25,
+            {restitution: 0.9, render: {fillStyle: '#b6f27d'}}
+        );
         Body.scale(ele, 1, 1);
         draw(ele);
     });
 
     col[1].addEventListener('click', () => {
-        let ele = createCircle(400, 250, 25, {restitution: 0.9, render: {fillStyle: '#f24'}});
+        let ele = createCircle(400, 250, 25,
+            {restitution: 0.9, render: {fillStyle: '#f24'}}
+        );
         Body.scale(ele, 2, 2);
         draw(ele);
     });
 
     col[2].addEventListener('click', () => {
-        let ele = createCircle(400, 250, 25, {restitution: 0.9, render: {fillStyle: '#f2f'}});
+        let ele = createCircle(400, 250, 25,
+            {restitution: 0.9, render: {fillStyle: '#f2f'}}
+        );
         Body.scale(ele, 3, 3);
         draw(ele);
     });
@@ -182,7 +199,6 @@ project.restitution = function () {
 
 //== FREEFALL --------------------------------------------------------------------------------->
 project.freeFall = function () {
-
     let render = Render.create({
         element: mid,
         engine: engine,
@@ -204,48 +220,35 @@ project.freeFall = function () {
 
     walls(800, 600);
 
-    let jupiter = createRect(85, 300, 160, 590, {
+    let planetOptions = {
         restitution: 0,
-        isSensor: true,
+        friction: 0,
+        frictionAir: 0,
         isStatic: true,
         render: {strokeStyle: '#C44D58', fillStyle: 'transparent', lineWidth: 1}
-    });
+    };
 
-    let saturn = createRect(245, 300, 160, 590, {
-        restitution: 0,
-        isSensor: true,
-        isStatic: true,
-        render: {strokeStyle: '#C44D58', fillStyle: 'transparent', lineWidth: 1}
-    });
+    let planetPos = [
+        {x: 160, y: 300, width: 2, height: 590, planetOptions},
+        {x: 320, y: 300, width: 2, height: 590, planetOptions},
+        {x: 480, y: 300, width: 2, height: 590, planetOptions},
+        {x: 640, y: 300, width: 2, height: 590, planetOptions},
+        {x: 800, y: 300, width: 2, height: 590, planetOptions},
+    ];
 
-    let earth = createRect(405, 300, 160, 590, {
-        restitution: 0,
-        isSensor: true,
-        isStatic: true,
-        render: {strokeStyle: '#C44D58', fillStyle: 'transparent', lineWidth: 1}
+    planetPos.forEach((v, k) => {
+        draw(Bodies.rectangle(planetPos[k].x,
+            planetPos[k].y, planetPos[k].width, planetPos[k].height, planetPos[k].planetOptions));
     });
-
-    let moon = createRect(565, 300, 160, 590, {
-        restitution: 0,
-        isSensor: true,
-        isStatic: true,
-        render: {strokeStyle: '#C44D58', fillStyle: 'transparent', lineWidth: 1}
-    });
-
-    let space = createRect(725, 300, 160, 590, {
-        restitution: 0,
-        isSensor: true,
-        isStatic: true,
-        render: {strokeStyle: '#C44D58', fillStyle: 'transparent', lineWidth: 1}
-    });
-
-    draw([jupiter, saturn, earth, moon, space]);
 
     let ball = createCircle(410, 100, 40, {
         restitution: 0.9,
         frictionAir: 0,
+        friction: 0,
+        mass: 1,
         render: {strokeStyle: '#C7F464', fillStyle: 'transparent', lineWidth: 1},
     });
+
     draw([ball]);
 
     Events.on(engine, 'afterUpdate', _.throttle(() => {
@@ -253,22 +256,22 @@ project.freeFall = function () {
 
             if (ballPosX >= 0 && ballPosX <= 160) {
                 world.gravity.y = 2.53;
-                ball.restitution = 0.2;
+                ball.restitution = 0.4;
             }
 
             if (ballPosX > 160 && ballPosX <= 320) {
                 world.gravity.y = 1.07;
-                ball.restitution = 0.5;
+                ball.restitution = 0.6;
             }
 
             if (ballPosX > 320 && ballPosX <= 480) {
                 world.gravity.y = 1;
-                ball.restitution = 0.6;
+                ball.restitution = 0.7;
             }
 
             if (ballPosX > 480 && ballPosX <= 640) {
                 world.gravity.y = 0.17;
-                ball.restitution = 1;
+                ball.restitution = 0.99;
             }
 
             if (ballPosX > 640 && ballPosX <= 800) {
@@ -282,7 +285,7 @@ project.freeFall = function () {
     let mouseConstraint = MouseConstraint.create(engine, {
         mouse,
         constraint: {
-            stiffness: 0.01,
+            stiffness: 0.2,
             angularStiffness: 1,
             render: {
                 visible: false,
@@ -310,11 +313,109 @@ project.freeFall = function () {
     }
 };
 
+
+//== Horizontal Fall ----------------------------------------------------------->
+project.horizontalFall = function () {
+    let render = Render.create({
+        element: mid,
+        engine: engine,
+        options: {
+            width: 800,
+            height: 600,
+            wireframes: true,
+            showVelocity: true,
+            background: '#333'
+        }
+    });
+
+    Render.run(render);
+
+    let runner = Runner.create();
+    Runner.run(runner, engine);
+
+    let botWall = wallBot(800, 600);
+    let rightWall = wallRight(800, 600);
+    let leftWall = wallLeft(800, 600);
+    draw([leftWall, botWall, rightWall]);
+
+    let xOs = createRect(400, 270, 800, 1,
+        {isStatic: true, collisionFilter: {mask: 0x0002}}
+    );
+    draw([xOs]);
+
+    world.gravity.y = 1;
+
+    let ball = createCircle(25, 265, 20,
+        {mass: 20, frictionAir: 0, friction: 0}
+    );
+
+    let polica = createRect(50, 290, 60, 10, {isStatic: true});
+
+    draw([ball, polica]);
+
+    let mouse = Mouse.create(render.canvas);
+    let mouseConstraint = MouseConstraint.create(engine, {
+        mouse,
+        constraint: {
+            stiffness: 0.2,
+            angularStiffness: 1,
+            render: {
+                visible: false,
+            }
+        }
+    });
+
+    draw(mouseConstraint);
+    render.mouse = mouse;
+
+    let hor = document.getElementById('horizontal');
+    let ver = document.getElementById('vertical');
+    let range = document.getElementById('range');
+    let rangeNum = document.getElementById('range-num');
+
+    range.addEventListener('input', () => {
+        rangeNum.innerText = range.value;
+    });
+
+    ver.addEventListener('click', () => {
+        // console.log(parseInt(range.value) * (Math.PI / 180))
+        let radian = parseInt(range.value) * (Math.PI / 180);
+        Body.applyForce(ball, {x: ball.position.x, y: ball.position.y},
+            {x: Math.cos(radian), y: -Math.sin(radian)}
+        );
+        console.log(ball.force);
+    });
+
+    Events.on(engine, 'afterUpdate', _.throttle(() => {
+            // console.log(ball.velocity);
+
+            let col = SAT.collides(ball, botWall).collided;
+            if (ball.position.x >= 60 && mouseConstraint.mouse.button === -1 && !col) {
+                let dot = createRect(ball.position.x, ball.position.y, 1, 1,
+                    {isStatic: true, collisionFilter: {mask: 0x0002}, render: {fillStyle: '#fff'}});
+                draw(dot);
+            }
+        }, 1)
+    );
+
+
+    Render.lookAt(render, {
+        min: {x: 0, y: 0},
+        max: {x: 800, y: 600},
+    });
+
+    return {
+        engine,
+        runner,
+        render,
+        canvas: render.canvas,
+        stop: function () {
+            Matter.Render.stop(render);
+            Matter.Runner.stop(runner);
+        }
+    }
+};
+
 // project.restitution();
-project.freeFall();
-
-const clear = document.getElementById('clearWorld');
-
-clear.addEventListener('click', function () {
-    World.clear(engine.world);
-});
+// project.freeFall();
+project.horizontalFall();
